1ddb71c994a708745f7db88e5e9ac9e49f8bfeac
Charles <105826694+FreeCharles02@users.noreply.github.com>
2024-06-22 02:12:48 -0500

    Switch case update

--- Program.cs
+++ Program.cs
@@ -9,13 +9,26 @@ static void Main(string[] args) {
 
 Console.WriteLine("You're lost in the magic woods an elf walks up to you in the dark and asks for you to come with them to shelter, what do you do?:");
 Console.WriteLine("Options: \n 1. Go with Elf \n 2. Punch elf \n 3. Run from elf");
-var userinput = Console.ReadLine();
-var choice = int.Parse(userinput);
+var userInput = Console.ReadLine();
+var choice = int.Parse(userInput);
 
-if (choice == 1) {
-Console.WriteLine("You go with the elf, he takes you to his little humble abode in the side of a tree he cooks you a bowl of porridge and shows you where you will be sleeping");
-Console.WriteLine("Options: \n 1. Happily Eat \n 2. Go to Sleep \n 3. Escape secretly");
-}
+switch (choice)
+{
+   case 1:  
+   Console.WriteLine("You go with the elf back to his humble abode in the side of a stump");
+   break;
+
+    case 2: 
+        Console.WriteLine("You punch the elf out of shock! The elf curses you and leaves you in the dark magic woods alone");
+            break;
+
+                case 3:
+                   Console.WriteLine("Stranger danger! You sprint in the other direction and can only hear the elf pleading with you to not leave him");
+                        break;
+                    default: 
+                    Console.WriteLine("Sorry! That is not an option...");
+                    break;
+            }
         }
     }
 };
